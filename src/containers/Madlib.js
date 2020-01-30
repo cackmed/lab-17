827fbbe618daba02a0867a251eabfd4122e7555b
Caleb Pendergraft <cackmed@gmail.com>
2020-01-30 15:55:27 -0800

    user input  being added to madlib

--- src/containers/Madlib.js
+++ src/containers/Madlib.js
@@ -5,30 +5,48 @@ import Result from '../components/madlib/Result';
 export default class Madlib extends Component{
   state = {
     showResult: false,
-    enteredword: {},
+    1: '',
+    2: '',
+    3: '',
+    4: '',
+    5: '',
+    6: '',
+    7: '',
+    8: '',
+    9: '',
+    10: '',
+    11: '',
+    12: '',
     words: []
   }
 
   handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value })
-    ;
-    console.log(target.value);
+    this.setState({ [target.name]: target.value });
   }
 
   toggleResult = () =>
-    this.setState(state => ({ ...state, showResult: !state.showResult }));
+    this.setState(state => ({ ...state, showResult: !state.showResult })
+    );
 
   handleSubmit = event => {
     event.preventDefault();
+    
+    this.setState({
+      words:[this.state[1], this.state[2], this.state[3], this.state[4], this.state[4], this.state[5], this.state[6], this.state[7], this.state[8], this.state[9], this.state[10], this.state[11], this.state[12]]
+    });
+
+    console.log(this.state);
+    console.log(this.state.words);
+
     this.toggleResult();
   }
 
   render() {
-    const { showResult } = this.state;
+    const { showResult, words } = this.state;
     return (
       <>
         {!showResult && <Form onSubmit={this.handleSubmit} onChange={this.handleChange} />}
-        {showResult && <Result words={[]} closeResult={this.toggleResult} />}
+        {showResult && <Result words={words} closeResult={this.toggleResult} />}
       </>
     );
   }
