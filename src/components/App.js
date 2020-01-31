d497d36395868d69423bbd2ca0dccfd1f28720a3
Caleb Pendergraft <cackmed@gmail.com>
2020-01-30 16:09:44 -0800

    footer and header componets added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Madlib from '../containers/Madlib';
 
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <section>
+          <h1>Madlibs</h1>
+          <h2>Fill out the fields below and click the button to load the madlib</h2>
+        </section>
+      </header>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <section>
+          <p>All writing is contributed by Alchemy Code School</p>
+        </section>
+      </footer>
+    );
+  }
+}
+
 export default function App() {
-  return <Madlib />;
+  return (  
+    <>
+      <Header />
+      <Madlib />
+      <Footer />
+    </>
+  );
 }
